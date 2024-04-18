2234cd1a78cbbc253a0234c647fad5945e7983dd
knot41 <2410580994@qq.com>
2024-04-18 17:29:05 +0800

    对类Picture进行了补充

--- patrol_line/include/picture.hpp
+++ patrol_line/include/picture.hpp
@@ -6,10 +6,14 @@ using namespace cv;
 
 class Picture{
 public:
-    double getAngle(Mat picture,Point flag1,Point flag2);
+    double getAngle(Point flag1,Point flag2);
     double getDistance(Mat picture,Point flag1,Point flag2);
-    double line_length(Point pt1,Point pt2);
+    double line_Length(Point pt1,Point pt2);
     void drawLine(Mat img,double rosw,double cols,vector<Vec2f> line);
+    void Prosess();
+    void Patrol_line();
 
+    Mat img;
+    Point flag1,flag2; 
     double angle,distance;
 };
