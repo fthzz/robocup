d65bab5d98ee12331d91ba6431f320c58c7b0e3e
knot41 <2410580994@qq.com>
2024-04-18 17:29:41 +0800

    修改了主函数

--- patrol_line/src/main.cpp
+++ patrol_line/src/main.cpp
@@ -6,24 +6,11 @@ using namespace cv;
 Point flag1,flag2;
 
 int main(){
-    Mat img = imread("the position of the picture",0);
-    resize(img,img,Size(0,0),0.2,0.2,INTER_LINEAR);
-    Mat img1,img2,kernel;
-    threshold(img,img1,200,255,THRESH_BINARY);
-    kernel = getStructuringElement(MORPH_RECT,Size(5,5));
-    morphologyEx(img1,img1,MORPH_OPEN,kernel);
-    morphologyEx(img1,img1,MORPH_CLOSE,kernel);
-    threshold(img1,img2,10,200,THRESH_BINARY);
-    vector<Vec2f>lines1;
-    vector<Vec2f>lines2;
-    HoughLines(img2,lines1,1,CV_PI/180,300);
-    Picture src;
-    src.drawLine(img2,img2.rows,img2.cols,lines1);
-    imshow("img2",img2);
-    waitKey(0);
-    src.angle = src.getAngle(img2,flag1,flag2);
-    src.distance = src.getDistance(img2,flag1,flag2);
-    printf("当前图片白线与y方向的偏离角度为 %lf 度\n",src.angle);
-    printf("图片竖直后白线与y轴的距离为 %lf 像素",src.distance);
+    Picture src1;
+    src1.Patrol_line();
+    auto a = src1.angle;
+    auto b = src1.distance;
+    printf("图像src1的白线与竖直方向的角度为%lf度\n",a);
+    printf("图像src1的白线矫正后与中间竖直直线的距离为%lf像素\n",b); 
     return 0;
 }
